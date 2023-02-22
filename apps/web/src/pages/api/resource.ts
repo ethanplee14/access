564906260f35a0bd20b4b6d669e66b77d2f2c155
BuildTools <ethanplee14@gmail.com>
2023-02-22 15:50:11 -0500

    feat: allowed upload files as a resource
    Uploaded files go to a self hosted server with a custom go file server
    system. Had to make some changes to nextjs api body parser to prevent
    corruption and limits.
    
    Updated daisyui for file input component.
    
    Body can't be forwarded when sending the response through for some
    reason. Made a stream reader and read in the nextjs api endpoint and
    front-end

--- apps/web/src/pages/api/resource.ts
+++ apps/web/src/pages/api/resource.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession as getServerSession } from "next-auth";
+import { signOut } from "next-auth/react";
+import { stdout } from "process";
+import { decodeReadableStream } from "../../utils/string";
 import { authOptions as nextAuthOptions } from "./auth/[...nextauth]";
 
 const createResource = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,22 +15,35 @@ const createResource = async (req: NextApiRequest, res: NextApiResponse) => {
     });
     return;
   }
+
   /* THIS IS A SECURITY RISK!!!!!
     right now just forwarding the userId clint side straight to be stored to the back-end. This means if someone's clever enough on the front-end, 
     they can send files to other people's resource folders. Trying to figure out how to transform multipart/form-data before it's forwarded. 
     Will keep it like this until further investigations. 
   */
-  const uploadRes = await fetch("http://107.155.121.13:8080/upload", {
+  const FS_URL = "http://107.155.121.13:8080";
+  const uploadRes = await fetch(FS_URL + "/upload", {
     method: "POST",
     headers: {
       "Content-Type": req.headers["content-type"] ?? "",
       Authorization: "Bearer " + process.env.FS_TOKEN,
     },
-    body: req.body,
+    body: req as any,
   });
 
-  if (!uploadRes.ok) throw new Error("It doesn't work");
-  res.send("ok");
+  if (!uploadRes.ok || !uploadRes.body)
+    throw new Error("Failed to read upload response");
+
+  const resPath = await decodeReadableStream(uploadRes.body);
+  res.send(FS_URL + "/res/" + resPath);
 };
 
 export default createResource;
+
+export const config = {
+  api: {
+    bodyParser: false,
+    // body parser malforms the data being forwarded and corrupts data
+    // Setting to false to keep it, will need to check file upload size manually.
+  },
+};
